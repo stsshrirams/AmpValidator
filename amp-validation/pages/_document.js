5b80c21d7c88c0e381b265a34fae355b565f184b
stsshrirams <77784064+stsshrirams@users.noreply.github.com>
2024-11-26 13:34:56 +0530

    sitte updated

--- amp-validation/pages/_document.js
+++ amp-validation/pages/_document.js
@@ -3,10 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <meta name="google-site-verification" content="XSc9ZG1kZTI963FEsY-fzk9CNqhgMkh_e36P7aJC" />
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
       <Head />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
+      <meta name="google-site-verification" content="XSc9ZG1kZTI963FEsY-fzk9CNqhgMkh_e36P7aJC" />
       <body>
         <Main />
         <NextScript />
