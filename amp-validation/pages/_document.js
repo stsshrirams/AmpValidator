cd8c28bd5c319df10bba1df09bb737a418ce1bfa
stsshrirams <77784064+stsshrirams@users.noreply.github.com>
2024-11-30 10:20:07 +0530

    tawk added

--- amp-validation/pages/_document.js
+++ amp-validation/pages/_document.js
@@ -3,12 +3,36 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-      <meta name="google-site-verification" content="XSc9ZG1kZTI963FEsY-fzk9CNqhgMkh_e36P7aJC" />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossOrigin="anonymous"
+        />
+        <meta
+          name="google-site-verification"
+          content="XSc9ZG1kZTI963FEsY-fzk9CNqhgMkh_e36P7aJC"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/674a98f92480f5b4f5a60802/1idtmb4ge';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </Html>
   );
